e121efd43dd47804109385bdf99cb9a8d3293a3b
echo <64895612+ech0-hi@users.noreply.github.com>
2026-03-24 10:36:43 +0100

    main.cpp

--- main.cpp
+++ main.cpp
@@ -18,6 +18,38 @@ bool isCommonPassword(string password)
     return false;
 }
 
+//basic suggestion gen 
+string generateSuggestion(string password)
+{
+    string suggestion=password;
+
+    if(suggestion.length()<8) suggestion+="123!";
+
+    bool hasUpper=false, hasDigit=false, hasSpecial=false;
+
+    for(char c:suggestion)
+    {
+        if(isupper(c)) hasUpper=true;
+        if(isdigit(c)) hasDigit=true;
+        if(ispunct(c)) hasSpecial=true;
+    }
+
+    if(!hasUpper) suggestion[0]=toupper(suggestion[0]);
+    if(!hasDigit) suggestion+="9";
+    if(!hasSpecial) suggestion+="!";
+
+    return suggestion;
+}
+
+//crack time estimate
+string crackTime(int score)
+{
+    if(score<=2) return "instantly (very weak)";
+    else if(score==3) return "a few minutes";
+    else if(score==4) return "a few hours";
+    else return "years (strong)";
+}
+
 int main() 
 {
     string password;
@@ -29,7 +61,7 @@ int main()
     bool hasDigit=false;
     bool hasSpecial=false;
 
-    for (char c:password) 
+    for(char c:password) 
     {
         if(isupper(c)) hasUpper=true;
         else if(islower(c)) hasLower=true;
@@ -51,10 +83,30 @@ int main()
     else if(score<=4) cout<<"strength: medium :|\n";
     else cout<<"strength: strong :)\n";
 
+    //feedback
+    cout<<"\nissues:\n";
+
+    if(password.length()<8) cout<<"-> too short (min 8 characters)\n";
+    if(!hasUpper) cout<<"-> missing uppercase letter\n";
+    if(!hasLower) cout<<"-> missing lowercase letter\n";
+    if(!hasDigit) cout<<"-> missing number\n";
+    if(!hasSpecial) cout<<"-> missing special character\n";
+
+    if(score==5) cout<<"- no major issues detected\n";
+
+    //common password warning
     if(isCommonPassword(password)) 
     {
         cout<<"⚠ warning: this is a common password!!!!!!!\n";
     }
+
+    //suggestion
+    cout<<"\nsuggested password:\n";
+    cout<<generateSuggestion(password)<<endl;
+
+    //crack time
+    cout<<"\nestimated crack time: ";
+    cout<<crackTime(score)<<endl;
 
     return 0;
 }
